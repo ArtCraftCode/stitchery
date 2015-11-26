817e6861889fef4f55925ccfd3948c34f3feaaab
Liz Abinante <eabinante@gmail.com>
2015-11-26 12:50:09 -0800

    pattern show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def pretty(float)
+    whole = float.to_i
+    remainder = float - whole
+    if remainder < 0.1
+      "#{whole}"
+    else
+      remainder_fraction = remainder.to_s.to_r
+      string = "<div class='fraction-display'>"
+      string += "#{whole} " if whole != 0
+      string += "<span class='fraction'>#{remainder_fraction}</span>"
+      string += " <span class='float'>(#{float})</span></div>"
+    end
+  end
 end
