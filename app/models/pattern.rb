df113740156f05e64fc29ac51bdedc95be19bfb1
Liz Abinante <eabinante@gmail.com>
2015-11-26 12:29:42 -0800

    pattern library page, start on pattern form

--- app/models/pattern.rb
+++ app/models/pattern.rb
@@ -1,3 +1,18 @@
 class Pattern < ActiveRecord::Base
   has_many :variants
+
+  def self.TYPES
+    [
+      'accessory',
+      'dress',
+      "men's",
+      'pants/shorts',
+      'skirt',
+      'home',
+      'swimwear',
+      'tops',
+      'undergarments',
+      'outerwear'
+    ]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151126192452) do
+ActiveRecord::Schema.define(version: 20151126202150) do
 
   create_table "patterns", force: :cascade do |t|
     t.string   "name"
     t.boolean  "digital"
     t.boolean  "cut"
-    t.string   "type"
+    t.string   "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
